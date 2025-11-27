3ff415e78ac6b3ac6659dd1a4a1bbf548989db94
Praxcedes <praxcedeskabeya@gmail.com>
2025-11-27 21:02:32 +0300

    app.jsx,login.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ import WorkoutDetail from './pages/WorkoutDetail';
 // Signup import removed since it is handled inside Login.jsx
 
 // Styles
-import './styles/login.css'; // Your Login styles
+import "./styles/Login.css"; // OK
 // import './styles/main.css'; // Global styles
 
 // This component handles hiding the Navbar on the Login page AND Dashboard
