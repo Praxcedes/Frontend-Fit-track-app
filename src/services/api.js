afef7ae345c24adc341b6a4731a0a027ed89cf0d
Davemuriu <dmuriu0@gmail.com>
2025-12-01 17:50:24 +0300

    CHORE: Update API base URL to point to the correct backend service

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://fittrack-api.onrender.com',
+    baseURL: 'https://backend-fit-track-app.onrender.com',
     headers: {
         'Content-Type': 'application/json',
     },
